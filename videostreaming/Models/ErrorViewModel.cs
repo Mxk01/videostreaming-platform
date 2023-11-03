4ffef5ba10f4d3d5af6b353dee69927b29cb5572
Robert M <61979889+Mxk01@users.noreply.github.com>
2023-11-03 10:27:18 +0200

    Add project files.

---
+++ videostreaming/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace videostreaming.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ videostreaming/Services/IFileService.cs
@@ -0,0 +1,8 @@
+﻿namespace videostreaming.Services
+{
+    public interface IFileService
+    {
+        Tuple<int, string> SaveImage(IFormFile imageFile);
+        public bool DeleteImage(string fileName);
+    }
+}
